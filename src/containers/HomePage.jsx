8133532618a0d2c47648d117e6e337f77f557743
matisantillan11 <matias1.santillan@gmail.com>
2021-08-18 23:30:49 -0300

    working on about section

--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -7,13 +7,29 @@ import '../assets/styles/containers/HomePage.css'
 
 export default () =>{
     return(
-        <div>
-            <Navbar/>
-            
-            <div className="MyDescription_container">
-                <img className ="MyDescription_container-image" src={Me} alt="Matias Santillan"/>
-            </div>   
+        <div className="home_page">
+            <Navbar/> 
 
+            <div id="hero">
+                <div className="hero__text-container">
+                    <h3 className="slogan">Hey, soy Matias Santillan</h3>
+                    <p>Fullstack developer y apasionado por la tecnología</p>
+                </div>
+
+                <a className="hero_toaction"href="">Más sobre mi</a>
+            </div>
+            <div id="about">
+            
+                <h2 className="WhoIam_container-title">
+                    Quien soy? 
+                </h2>
+                <p className="WhoIam_container-body">
+               Soy Desarrollador de Software que le encanta crear con tecnologías webs</p>
+    
+
+                    
+            </div>
+           
         </div>
     )
 }
